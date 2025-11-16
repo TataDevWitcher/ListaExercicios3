f77bb76bbb68321b245e897ffa77769b5e3b84fc
TataDevWitcher <vitorialadyletal@gmail.com>
2025-11-16 20:09:50 -0300

    Adicionando solução e primeiro exercício

---
+++ ExerciciosBasicos3/Exercicio1/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Validador de senha correta*");
+            Console.WriteLine("Tente descobrir a senha secreta. Dica: Já passamos por esse ano, entre 1990 e 2025.");
+            Console.WriteLine("Insira a senha: ");
+
+            int senhaSecreta = int.Parse(Console.ReadLine());
+
+            while (senhaSecreta != 2002)
+            {
+                Console.WriteLine("A senha é inválida");
+                senhaSecreta = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido!");
+            Console.WriteLine("CARREGANDO...");
+
+        }
+    }
+}
